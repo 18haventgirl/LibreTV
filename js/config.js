75c8b0d4f4f4db4c6ef74cfe8f67073c44dc82aa
Faye wong <2953267556@qq.com>
2025-11-17 19:26:59 +0800

    Merge remote-tracking branch 'origin/1258movie_01' into 1258movie_01

--- js/config.js
+++ js/config.js
@@ -26,6 +26,11 @@ const API_SITES = {
         name: '电影天堂资源',
         detail: 'http://caiji.dyttzyapi.com', 
     },
+    dyttzy1: {
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        name: '电影天堂云资源',
+        detail: 'http://caiji.dyttzyapi.com',
+    },
     ruyi: {
         api: 'https://cj.rycjapi.com/api.php/provide/vod',
         name: '如意资源',
